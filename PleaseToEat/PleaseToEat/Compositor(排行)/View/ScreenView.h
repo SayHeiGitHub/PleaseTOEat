9e6bdf86682024fa54c38877f93dda94d37df384
lanou3g <18209185257@163.com>
2016-04-21 10:10:58 +0800

    加约束

---
+++ PleaseToEat/PleaseToEat/Compositor(排行)/View/ScreenView.h
@@ -0,0 +1,13 @@
+//
+//  ScreenView.h
+//  PleaseToEat
+//
+//  Created by lanou3g on 16/4/21.
+//  Copyright © 2016年 HeiCoder_OR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScreenView : UIView
+
+@end
---
+++ PleaseToEat/PleaseToEat/Compositor(排行)/View/ScreenView.m
@@ -0,0 +1,21 @@
+//
+//  ScreenView.m
+//  PleaseToEat
+//
+//  Created by lanou3g on 16/4/21.
+//  Copyright © 2016年 HeiCoder_OR. All rights reserved.
+//
+
+#import "ScreenView.h"
+
+@implementation ScreenView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
